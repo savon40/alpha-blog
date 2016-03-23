ced3d6afe68781eba320f04a20e281a0b68f0569
savon40 <savon40@gmail.com>
2016-03-23 16:29:15 +0000

    new, show, edit, index and links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,36 @@ class ArticlesController < ApplicationController
    def create
       #render plain: params[:article].inspect
       @article = Article.new(article_params)
-      @article.save
-      redirect_to articles_show(@article)
+     
+      if @article.save
+         #do something
+         flash[:notice] = 'Article was successfully created'
+         redirect_to article_path(@article)
+      else 
+         render 'new'
+      end
+   end
+   
+   def show
+      @article = Article.find(params[:id])
+   end
+   
+   def edit
+      @article = Article.find(params[:id])
+   end
+   
+   def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+         flash[:notice] = 'Article was successfully updated'
+         redirect_to article_path(@article)
+      else 
+         render 'edit'
+      end
+   end
+   
+   def index
+      @articles = Article.all
    end
    
    private def article_params
